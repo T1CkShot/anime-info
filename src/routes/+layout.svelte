f1ab5777aaaf3f54610c09cc8a47f5aa07d2745a
T1CkShot <fatta.mmg@gmail.com>
2023-09-24 20:19:18 +0600

    set root layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../global.css';
+	import NavBar from '../lib/components/NavBar.svelte';
+	let navH;
+</script>
+
+<NavBar bind:clientHeight={navH} />
+<div style="top:91px">
+	<slot />
+</div>
+
+<style>
+	div {
+		position: relative;
+	}
+</style>
