76818b93b354817971f9b09002458f9306ee498f
T1CkShot <fatta.mmg@gmail.com>
2023-09-24 20:15:35 +0600

    create nav bar component

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,52 @@
+<script>
+	import '../../global.css';
+	import Button from './Button.svelte';
+</script>
+
+<nav>
+	<div class="main">
+		<img src="../../../static/favicon.svg" alt="Logo" />
+		<div class="search">
+			<input type="text" name="searchbar" placeholder="Search..." />
+			<Button value="Search" />
+		</div>
+	</div>
+</nav>
+
+<style>
+	.main {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.search {
+		margin: 15px 8px;
+		display: flex;
+		align-items: center;
+		gap: 12px;
+	}
+
+	img {
+		width: auto;
+		height: 50px;
+	}
+	input {
+		border-radius: 8px;
+		background: var(--background-light);
+		box-shadow: var(--background-shadow) 3px 2px 7px;
+		border: none;
+		padding: 9px;
+		min-width: 18em;
+		color: var(--text);
+	}
+
+	nav {
+		position: fixed;
+		width: 100%;
+		top: 0px;
+		z-index: 3;
+		padding: 14px;
+		box-sizing: border-box;
+		background-color: var(--background);
+	}
+</style>
