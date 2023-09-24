631cb6c00e7b7abb222f725807bb21d4dddfee4e
T1CkShot <fatta.mmg@gmail.com>
2023-09-24 20:15:14 +0600

    create button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,23 @@
+<script>
+	import '../../global.css';
+	export let type = 'button';
+	export let aherf = '#';
+	export let value = 'Button';
+</script>
+
+<div>
+	<a href={aherf}><input {type} {value} /></a>
+</div>
+
+<style>
+	div {
+		display: flex;
+	}
+
+	input {
+		background: var(--primary);
+		border: none;
+		border-radius: 8px;
+		padding: 8px 18px;
+	}
+</style>
