4c885729699654d7dd63fe2c3859bd4c4dbd6aec
T1CkShot <fatta.mmg@gmail.com>
2023-11-28 22:12:16 +0600

    add button & overlay to hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../global.css';
+	import Button from '../lib/components/Button.svelte';
 </script>
 
 <div class="main">
@@ -11,11 +12,12 @@
 				</h1>
 				<p style="line-height: 1.5em; font-size: 0.9em;">
 					Check out my humble attempt at an anime search website, made using SvelteKit. Let's be
-					honest the world doesn't need yet another anime search site? But hey, curiosity got the
+					honest the world doesn't need yet another anime search site. But hey, curiosity got the
 					best of me, and I decided to give it a go. No frills, just a basic exploration of
 					<a href="https://kit.svelte.dev/">SvelteKit</a>. Sometimes, simplicity is the best
 					teacher!
 				</p>
+				<Button aherf="https://github.com/T1CkShot/anime-info" value="GitHub" size="big" />
 			</div>
 		</div>
 	</div>
@@ -33,7 +35,9 @@
 		width: 100%;
 		font-size: 1.4em;
 		text-shadow: var(--background) 4px 4px 40px;
-		background: center/cover url('https://images2.alphacoders.com/743/743487.jpg');
+		background-image: linear-gradient(125deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0)),
+			url('https://images2.alphacoders.com/743/743487.jpg');
+		background-position: center;
 	}
 	.hero-section a {
 		border-bottom: 3px solid var(--primary);
