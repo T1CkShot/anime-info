4d7eece7aac9cf69b8cfadc15a54a955f8843933
T1CkShot <fatta.mmg@gmail.com>
2023-11-28 21:39:33 +0600

    add sizes and style

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -3,10 +3,13 @@
 	export let type = 'button';
 	export let aherf = '#';
 	export let value = 'Button';
+	export let secondary = false;
+	// sizes: "small", "medium", "big"
+	export let size = 'medium';
 </script>
 
 <div>
-	<a href={aherf}><input {type} {value} /></a>
+	<a href={aherf}><input class={size} class:secondary {type} {value} /></a>
 </div>
 
 <style>
@@ -19,6 +22,20 @@
 		color: var(--text);
 		border: none;
 		border-radius: 8px;
+	}
+	.secondary {
+		background: var(--text);
+		color: var(--background);
+	}
+	.small {
 		padding: 8px 18px;
+	}
+	.medium {
+		padding: 10px 22px;
+	}
+	.big {
+		padding: 12px 27px;
+		font-size: 0.7em;
+		font-weight: 700;
 	}
 </style>
