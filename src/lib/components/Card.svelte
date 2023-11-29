b8029bb12f0b85706818b0d66dc0c3056151ee57
T1CkShot <fatta.mmg@gmail.com>
2023-11-29 21:54:41 +0600

    make card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,37 @@
+<script>
+	import '../../global.css';
+	export let title = 'Attack on Titan';
+	export let cover =
+		'https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg';
+	export let rating = 84;
+</script>
+
+<div class="card">
+	<img src={cover} alt={`${title} cover`} />
+	<div class="info">
+		<p>{title}</p>
+		<p class="rating">{Math.round((rating / 20) * 10) / 10}</p>
+	</div>
+</div>
+
+<style>
+	.card {
+		max-width: 200px;
+	}
+	.rating {
+		color: var(--primary);
+		font-weight: bold;
+		font-size: 1em;
+	}
+	img {
+		width: 100%;
+		height: auto;
+		border-radius: 12px;
+	}
+	p {
+		font-family: 'Roboto';
+		font-weight: 400;
+		font-size: 1.2em;
+		margin: 5px;
+	}
+</style>
